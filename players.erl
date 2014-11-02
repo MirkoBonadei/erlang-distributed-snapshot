494052afb792a3ad9e5d3d834c321d7898aefbf5
Giorgio Sironi <info@giorgiosironi.com>
2014-11-02 11:03:23 +0100

    Built a ring of 3 processes, plus an output one that gets all the messages

--- players.erl
+++ players.erl
@@ -1,8 +1,22 @@
 -module(players).
--export([setup/0, output/0]).
+-export([setup/0, output/0, decrementing_player/1]).
 
 output() -> receive
                 M -> io:fwrite(M)
             end.
 
-setup() -> spawn(players, output, []).
+decrementing_player(Targets) -> receive 
+    {target, T} -> decrementing_player(lists:append(Targets, [T]))
+end.
+
+setup() ->
+    O = spawn(players, output, []),
+    register(output, O),
+    P1 = spawn(players, decrementing_player, [[O]]),
+    register(entry, P1),
+    P2 = spawn(players, decrementing_player, [[O]]),
+    P3 = spawn(players, decrementing_player, [[O]]),
+    P1 ! {target, P2},
+    P2 ! {target, P3},
+    P3 ! {target, P1}
+    .
