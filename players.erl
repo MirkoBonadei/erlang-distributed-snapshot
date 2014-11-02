4125f5be0e64141a44ed49100d770199d87eeb21
Giorgio Sironi <info@giorgiosironi.com>
2014-11-02 11:26:46 +0100

    Ring processes decrement the value and pass it to the next process in the ring

--- players.erl
+++ players.erl
@@ -2,21 +2,32 @@
 -export([setup/0, output/0, decrementing_player/1]).
 
 output() -> receive
-                M -> io:fwrite(M)
+                M -> io:fwrite("~w~n", [M]),
+                output()
             end.
 
 decrementing_player(Targets) -> receive 
-    {target, T} -> decrementing_player(lists:append(Targets, [T]))
+    {target, Target} -> decrementing_player(lists:append(Targets, [Target]));
+    {token, Value} ->
+        Decremented = Value - 1,
+        if
+            Decremented > 0 ->
+                lists:map(fun(Target) -> Target ! {token, Decremented} end, Targets);
+            true -> true
+        end,
+        decrementing_player(Targets)
 end.
 
 setup() ->
     O = spawn(players, output, []),
-    register(output, O),
+    register(output_pid, O),
     P1 = spawn(players, decrementing_player, [[O]]),
-    register(entry, P1),
+    register(entry_pid, P1),
     P2 = spawn(players, decrementing_player, [[O]]),
     P3 = spawn(players, decrementing_player, [[O]]),
     P1 ! {target, P2},
     P2 ! {target, P3},
     P3 ! {target, P1}
     .
+
+
