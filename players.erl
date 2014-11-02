a976174c90d5775a4b82a72b118477437a706352
Giorgio Sironi <info@giorgiosironi.com>
2014-11-02 15:39:44 +0100

    Big changes
    Every process tracks the remaining channels during snapshot
    taking (from which he has not yet received the marker).
    Funnel only gets one message telling him snapshot's finished, and he
    asks one by one the processes to collect the state before resetting them
    to the working state.
    The termination property (one process has the value 1 as LastSeen) is
    correctly detected after a few rounds.

--- players.erl
+++ players.erl
@@ -2,18 +2,21 @@
 -export([setup/0, snapshot/0, decrementing_player/0, decrementing_player/1, start/1, funnel/1]).
 
 decrementing_player() -> decrementing_player([]). 
-decrementing_player(Targets) -> decrementing_player(Targets, undefined, {working}). 
-decrementing_player(Targets, LastSeen, State) -> receive 
+decrementing_player(Targets) -> decrementing_player(Targets, 100, {working}). 
+decrementing_player(Targets, LastSeen, State) -> 
+    io:fwrite("State of ~w: ~w~n", [self(), State]),
+    receive 
     {target, Target} -> decrementing_player(
                           lists:append(Targets, [Target]),
                           LastSeen,
                           State
                         );
-    {token, Value} ->
+    {token, Value, Sender} ->
         Decremented = Value - 1,
         if
             Decremented > 0 ->
-                lists:map(fun(Target) -> Target ! {token, Decremented} end, Targets);
+                [Next|_] = Targets,
+                Next ! {token, Decremented, self()};
             true -> true
         end,
         if 
@@ -23,74 +26,101 @@ decrementing_player(Targets, LastSeen, State) -> receive
                   Value,
                   {
                    taking_snapshot,
-                   lists:append(element(2, State), [{token, Value}])
+                   lists:append(element(2, State), [{token, Value, Sender}]),
+                   element(3, State)
                   }
                  );
             true ->
                 decrementing_player(Targets, Value, State)
         end;
-    {marker, Funnel} -> 
+    {marker, Funnel, Sender} -> 
         if 
-            element(1, State) == took_snapshot ->
-                decrementing_player(
-                  Targets,
-                  LastSeen,
-                  {working}
-                );
             element(1, State) == taking_snapshot ->
-                lists:map(fun(Target) -> Target ! {marker, Funnel} end, Targets),
-                Snapshot = {snapshot, element(2, State)},
-                Funnel ! Snapshot,
+                Snapshot = element(2, State),
+                Remaining = lists:filter(
+                    fun(Target) -> Target /= Sender end,
+                    element(3, State)
+                ),
+                io:fwrite("Remaining: ~w~n", [Remaining]),
+                if 
+                    length(Remaining) == 0 -> 
+                        Funnel ! {snapshot_terminated};
+                    true -> true
+                end,
                 decrementing_player(
                   Targets,
                   LastSeen,
-                  {took_snapshot}
+                  {taking_snapshot, Snapshot, Remaining}
                 );
+    
             element(1, State) == working ->
-                lists:map(fun(Target) -> Target ! {marker, Funnel} end, Targets),
+                lists:map(fun(Target) -> Target ! {marker, Funnel, self()} end, Targets),
+                Remaining = Targets,
                 decrementing_player(
                   Targets,
                   LastSeen,
-                  {taking_snapshot, [LastSeen]}
+                  {taking_snapshot, [LastSeen], Remaining}
                 )
-        end
+        end;
+    {tell_snapshot, Funnel} ->
+        % TODO: check State is taking_snapshot
+        Snapshot = element(2, State),
+        Funnel ! {snapshot, Snapshot},
+        decrementing_player(
+          Targets,
+          LastSeen,
+          {working, [LastSeen]}
+        )
 end.
 
 setup() ->
     P1 = spawn(players, decrementing_player, []),
-    register(entry_pid, P1),
+    register(entry_pid1, P1),
     P2 = spawn(players, decrementing_player, []),
+    register(entry_pid2, P2),
     P3 = spawn(players, decrementing_player, []),
+    register(entry_pid3, P3),
     P1 ! {target, P2},
+    P1 ! {target, P3},
     P2 ! {target, P3},
-    P3 ! {target, P1}
-    .
+    P2 ! {target, P1},
+    P3 ! {target, P1},
+    P3 ! {target, P2}.
+
 start([NAsString]) ->
     {N, ""} = string:to_integer(NAsString),
-    io:fwrite("~w~n", [N]),
-    whereis(entry_pid) ! {token, N}.
+    io:fwrite("Starting with: ~w~n", [N]),
+    whereis(entry_pid1) ! {token, N, self()}.
 
 snapshot() ->
-    Funnel = spawn(players, funnel, [3]),
-    whereis(entry_pid) ! {marker, Funnel}.
+    Funnel = spawn(players, funnel, [[whereis(entry_pid1), whereis(entry_pid2), whereis(entry_pid3)]]),
+    whereis(entry_pid1) ! {marker, Funnel, start}.
 
-funnel(NumberOfProcesses) -> funnel(NumberOfProcesses, []).
-funnel(NumberOfProcesses, CollectedSnapshots) ->
+funnel(Processes) ->
+    receive 
+            {snapshot_terminated} -> 
+                lists:map(fun(P) -> P ! {tell_snapshot, self()} end, Processes),
+                collect(Processes)
+    end.
+
+collect(Processes) ->  collect(Processes, []).
+collect(Processes, CollectedSnapshots) -> 
     if 
-        NumberOfProcesses == 0 -> 
-            Terminated = termination(CollectedSnapshots),
-            if
-                Terminated -> io:fwrite("Terminated!");
-                true -> snapshot(), funnel(length(CollectedSnapshots))
+        length(CollectedSnapshots) < length(Processes) ->
+            receive 
+                {snapshot, Snapshot} -> 
+                    io:fwrite("~w~n", [Snapshot]),
+                    collect(Processes, lists:append(CollectedSnapshots, [Snapshot]))
             end;
-        true -> receive 
-            SnapshotOfAProcess ->
-                io:fwrite("Snapshot[~w]: ~w~n", [self(), SnapshotOfAProcess]),
-                funnel(NumberOfProcesses - 1, lists:append(CollectedSnapshots, [SnapshotOfAProcess]))
-        end
+        true ->
+            Termination = termination(CollectedSnapshots),
+            if 
+                Termination -> true;
+                true -> snapshot()
+            end
     end.
 
 termination(CollectedSnapshots) -> 
     length(
-      lists:filter(fun(S) -> [State|_Messages] = element(2, S), State == 1 end, CollectedSnapshots)
+      lists:filter(fun(Snapshot) -> [State|_Messages] = Snapshot, State == 1 end, CollectedSnapshots)
      ) > 0.
